f4d85399c56b31a63d0a8f86835fcac64fb5baca
ZazaVader <121685561+ZazaVader@users.noreply.github.com>
2023-01-09 21:15:21 +0100

    Création du package GestionComptes et des classes AjouterAdmin et AjouterParticulier. Travail en cours.

---
+++ src/GestionComptes/AjouterAdmin.java
@@ -0,0 +1,38 @@
+package GestionComptes;
+
+import Affichage.Accueil;
+
+import java.util.Scanner;
+
+public class AjouterAdmin {
+    
+    //Authentification admin effectuée lors de l'accès au menu admin
+
+    //Vérification que le compte n'existe pas déjà en testant l'email
+    public static void verificationEmail(){
+
+
+    }
+
+        //System.out.println "Veuillez indiquer l'email de l'administrateur à ajouter"
+        //email = entree scanner 
+
+        //si email = email hasmaps comptes
+            //System.out.println "Cet email est déjà utilisé"
+            //close scanner
+            //Retour menu admin
+
+        //sinon
+            //System.out.println "Veuillez indiquer le mot de passe du compte administrateur"
+            //mdp =  entree scanner
+            //AjoutAdmin : new compte (email, mdp, admin) + ajout dans le hasmap comptes
+            public static void ajoutAdmin(){
+
+        
+            }
+            
+            //enregistrement de comptes.txt
+            //retour menu admin
+
+
+}
---
+++ src/GestionComptes/CreationCompteAdmin.java
@@ -0,0 +1,12 @@
+package GestionComptes;
+
+public class CreationCompteAdmin {
+    
+    //Authentification admin effectuée lors de l'accès au menu admin
+
+    //Vérification que le compte n'existe pas déjà en testant l'email
+
+        //System.out.println "Veuillez indiquer l'email de l'administrateur à ajouter"
+        //email = entree scanner 
+
+}
