4a13907e44c1846b7655d8e00c0effd533f07862
croosti <leducguillaume12@gmail.com>
2023-01-09 18:54:28 +0100

    Update Recherche.java
    Refonte des méthodes de recherche et appel à la class ResultatRecherche pour afficher les résultats

--- src/Recherche/Recherche.java
+++ src/Recherche/Recherche.java
@@ -1,37 +1,61 @@
 package Recherche;
 
-import Fichiers.LectureFichier;
+import Affichage.ResultatRecherche;
 import Utilisateurs.Particulier;
-
-import java.io.File;
 import java.util.*;
 
-
+/**
+ * Classe qui regroupe les méthodes de recherche dans l'annuaire
+ */
 public class Recherche {
 
-    public Particulier chercherParEmail(HashMap<String, Particulier> annuaire, Scanner sc){
-        System.out.println("Veuillez saisir un email à rechercher : ");
-        String email = sc.next();
+    //on instancie la class pour afficher les résultats de recherche
+    ResultatRecherche affRes = new ResultatRecherche();
 
-        System.out.println(annuaire.get(email));
+    /**
+     * Recherche par email.
+     * Si l'utilisateur ne saisit pas un input contenant un @, il est invité à recommencer
+     * Et affichage du resultat de recherche
+     * @param annuaire
+     * @param sc
+     */
+    public void chercherParEmail(HashMap<String, Particulier> annuaire, Scanner sc){
+        String email = "";
+        do{
+            System.out.println("Veuillez saisir un email à rechercher : ");
+            email = sc.next();
+        } while (!email.contains("@"));
 
-        return annuaire.get(email);
+        affRes.afficherResultatRecherche(annuaire.get(email));
     }
 
-    public List<Particulier> chercherParNom(HashMap<String, Particulier> annuaire, Scanner sc){
+    /**
+     * Recherche par Nom.
+     * L'utilisateur est invité à saisir un nom à rechercher dans l'annuaire.
+     * Le champ Particulier.getNom() est comparé à l'input de l'utilisateur
+     * Et affichage du resultat de recherche
+     * @param annuaire
+     * @param sc
+     */
+    public void chercherParNom(HashMap<String, Particulier> annuaire, Scanner sc){
         List<Particulier> liste;
 
         System.out.println("Veuillez saisir un nom à rechercher : ");
         String nom = sc.next();
         liste = annuaire.values().stream().filter(p -> p.getNom().equals(nom)).toList();
 
-        System.out.println(liste);
-
-        //return liste pour affichage ou appel affichage ICI ?
-        return liste;
+        affRes.afficherResultatRecherche(liste);
     }
 
-    public List<Particulier> chercherParProfil(HashMap<String, Particulier> annuaire, Scanner sc){
+    /**
+     * recherche par Profil
+     * L'utilisateur est invité à choisir un profil à rechercher (Auditeur, Enseignant ou Direction)
+     * Si l'input de l'utilisateur ne correspond pas à l'initiale d'un des choix, il est invité à recommencer.
+     * Et Affichage du resultat de recherche
+     * @param annuaire
+     * @param sc
+     */
+    public void chercherParProfil(HashMap<String, Particulier> annuaire, Scanner sc){
         List<Particulier> liste;
         boolean profilConforme = false;
         String profil;
@@ -53,8 +77,6 @@ public class Recherche {
         Particulier.Profil finalPro = pro;
         liste = annuaire.values().stream().filter(p -> p.getProfil() == finalPro).toList();
 
-        System.out.println(liste);
-
-        return liste;
+        affRes.afficherResultatRecherche(liste);
     }
 }
