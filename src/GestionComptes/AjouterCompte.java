8311fa0cc0fbf523ad8fde7982f7e517281e24e4
Andréa Vié <andreavie6329@gmail.com>
2023-01-14 00:44:52 +0100

    La vérification controleEnregistrement, nouvelle REGEX email et retour à l'ancienne logique

--- src/GestionComptes/AjouterCompte.java
+++ src/GestionComptes/AjouterCompte.java
@@ -1,13 +1,11 @@
 package GestionComptes;
 
-import Fichiers.EcritureFichier;
-import Utilisateurs.Compte.Role;
-import Utilisateurs.Particulier;
-
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.Date;
 
+import Fichiers.EcritureFichier;
+import Utilisateurs.*;
 import static Affichage.Accueil.*;
 
 /**
@@ -16,32 +14,41 @@ import static Affichage.Accueil.*;
 public class AjouterCompte {
     /**
      * Méthode pour créer un compte ainsi que les informations correspondantes dans l'annuaire
-     *
      * @param role Role de l'utilisateur à créer
-     * @return Réussite de l'ajout
+     * @return  Réussite de l'ajout
      * @throws IOException Erreur d'écriture sur les fichiers
      */
-    public static boolean creationCompte(Role role) throws IOException {
+    public static boolean creationCompte(Compte.Role role) throws IOException {
         String email = "", motDePasse = "",
                 nom = "", prenom = "", adressePostale = "",
                 dateNaissanceInput = "", profilInput = "";
-
         Date dateNaissance = null, dateAjout, dateMaj;
         Particulier.Profil profil = null;
 
         scannerClavier.nextLine();
-        while (!email.matches("^[a-z@.]*$") || email.isEmpty()) {
-            afficherVert("Veuillez indiquer l'email du compte " + role + " à ajouter :");
+        while (!email.matches("^(?=.{1,64}@)[\\p{L}0-9_-]+(\\.[\\p{L}0-9_-]+)*@"
+                + "[^-][\\p{L}0-9-]+(\\.[\\p{L}0-9-]+)*(\\.\\p{L}{2,})$") || email.isEmpty()) {
+            afficherVert("Veuillez indiquer l'email du compte " + role + " à ajouter (Laisser vide pour quitter le menu) :");
             email = scannerClavier.nextLine().toLowerCase();
-            if (comptes.get(email) != null) {
+            if (comptes.get(email) != null){
                 Affichage.Accueil.afficherRouge("Cet email est déjà associé à un compte. \nRetour au menu administrateur.");
-                return true;
+                return false;
+            } else if (email.isEmpty()) {
+                Affichage.Accueil.afficherRouge("Retour au menu administrateur.");
+                return false;
             }
         }
         while (!motDePasse.matches("^\\S*$") || motDePasse.isEmpty()) {
             afficherVert("Veuillez indiquer le mot de passe du Particulier à ajouter :");
             motDePasse = scannerClavier.nextLine();
         }
+
+        if (role == Compte.Role.Administrateur) {
+            Compte admin = new Compte(email, motDePasse, Compte.Role.Administrateur);
+            comptes.put(email, admin);
+            return controleEnregistrement(email, role);
+        }
+
         while (!nom.matches("^[A-zÀ-ú]*$") || nom.isEmpty()) {
             afficherVert("Veuillez indiquer le nom du Particulier à ajouter :");
             nom = scannerClavier.nextLine();
@@ -77,14 +84,20 @@ public class AjouterCompte {
         dateAjout = new Date(System.currentTimeMillis());
         dateMaj = new Date(System.currentTimeMillis());
         Particulier particulier = new Particulier(email, motDePasse, nom, prenom, adressePostale, dateNaissance, dateAjout, dateMaj, profil);
-        if (role == Role.Administrateur) {
-            particulier.setRole(Role.Administrateur);
-        }
+
         comptes.put(email, particulier);
         annuaire.put(email, particulier);
-        EcritureFichier.ecrireAnnuaire(annuaire, FICHIER_ANNUAIRE);
-        EcritureFichier.ecrireComptes(comptes, FICHIER_COMPTES);
-
-        return false;
+        return controleEnregistrement(email, role);
+    }
+    public static boolean controleEnregistrement(String email, Compte.Role role) throws IOException {
+        if ((comptes.get(email) != null)){
+            EcritureFichier.ecrireAnnuaire(annuaire, FICHIER_ANNUAIRE);
+            EcritureFichier.ecrireComptes(comptes, FICHIER_COMPTES);
+            afficherRouge(role + " ajouté !");
+            return true;
+        } else {
+            afficherRouge("Erreur lors de l'ajout du : " + role);
+            return false;
+        }
     }
 }
